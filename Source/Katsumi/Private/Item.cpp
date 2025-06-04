ea23903f5c1ef43c9858d5dd0004de713485c4ae
Askhat <abdirazaxm@gmail.com>
2025-06-03 21:37:57 -0400

    Create Item C++ Class & Open World Map

---
+++ Source/Katsumi/Private/Item.cpp
@@ -0,0 +1,16 @@
+#include "Item.h"
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
