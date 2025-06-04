6b8e2a146eedc09d894601522c45b4498d1ee0ca
Askhat <abdirazaxm@gmail.com>
2025-06-04 17:12:53 -0400

    Add Bird and Item Classes

---
+++ Source/Katsumi/Private/Items/Item.cpp
@@ -0,0 +1,32 @@
+#include "Items/Item.h"
+
+#include "Katsumi/DebugMacros.h"
+
+AItem::AItem()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMeshComponent"));
+	RootComponent = ItemMesh;
+}
+
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+float AItem::TransformedSin()
+{
+	return FMath::Sin(RunningTime * TimeConstant) * Amplitude;
+}
+
+float AItem::TransformedCos()
+{
+	return FMath::Cos(RunningTime * TimeConstant) * Amplitude;
+}
+
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	RunningTime += DeltaTime;
+}
---
+++ Source/Katsumi/Public/Items/Item.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class KATSUMI_API AItem : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AItem();
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trigonometry Parameters")
+	float Amplitude = 0.25f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trigonometry Parameters")
+	float TimeConstant = 5.f;
+
+	UFUNCTION(BlueprintPure, Category = "Trigonometry Parameters")
+	float TransformedSin();
+
+	UFUNCTION(BlueprintPure, Category = "Trigonometry Parameters")
+	float TransformedCos();
+
+	template <typename T>
+	T Avg(T A, T B);
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess = "true"))
+	float RunningTime = 0.f;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* ItemMesh;
+};
+
+template <typename T>
+T AItem::Avg(T A, T B)
+{
+	return (A + B) / 2;
+}
